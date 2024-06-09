4937d81dd17662572e6d499443d14c8cb5e332f4
rupa-vathi <157632655+rupa-vathi@users.noreply.github.com>
2024-06-09 15:51:13 +0530

    Delete MoviesAppTestPage directory

--- MoviesAppTestPage/src/test/java/TestRunner.java
+++
@@ -1,8 +0,0 @@
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.Listeners;
-
-@Listeners(MovieAppReport.class)
-@CucumberOptions(features = "src/test/resources/features")
-public class TestRunner extends AbstractTestNGCucumberTests {
-}
